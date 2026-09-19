066276ad9721445587150621100610b0404e5ecf
asicgn <cugenasiye@gmail.com>
2026-09-19 07:43:04 +0300

    Lesson 5: Dynamic (any) types

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,13 @@
 "use strict";
-//explicit types
-let character;
-let age;
-let isLoggedIn;
-// age = 'luigi'; we define age as a number, so we cannot assign a string to it
-age = 40;
-// isLoggedIn =25; same as above, we cannot assign a number to a boolean variable
-isLoggedIn = true;
-//arrays
-let ninjas = []; // we have to make sure that array is empty for push method to work
-ninjas.push('shaun');
-//union types
+let age = 25; // same as let age: any;
+age = true;
+console.log(age);
+age = 'yoshi';
+console.log(age);
+age = { name: 'luigi', belt: 'black' };
+console.log(age);
 let mixed = [];
-mixed.push(20);
-mixed.push('hello');
-mixed.push(true);
+mixed.push(5);
+mixed.push('mario');
+mixed.push(false);
 console.log(mixed);
-let uid;
-uid = '123';
-uid = 123;
-//objects
-let ninjaOne;
-ninjaOne = { name: 'yoshi', age: 30 };
-let ninjaTwo;
-ninjaTwo = { name: 'mario', age: 20, beltColour: 'black' }; // it has to have all the properties defined in the object type and not any other properties
--- sandbox.ts
+++ sandbox.ts
@@ -1,37 +1,17 @@
-//explicit types
-let character:string;
-let age: number;
-let isLoggedIn: boolean;
+let age: any = 25; // same as let age: any;
 
-// age = 'luigi'; we define age as a number, so we cannot assign a string to it
-age = 40;
+age = true;
+console.log(age);
+age = 'yoshi';
+console.log(age);
+age = {name: 'luigi', belt: 'black'};
+console.log(age);
 
-// isLoggedIn =25; same as above, we cannot assign a number to a boolean variable
-isLoggedIn = true;
+let mixed: any[] = [];
 
-//arrays
-let ninjas: string[] = []; // we have to make sure that array is empty for push method to work
-ninjas.push('shaun');
-
-//union types
-let mixed: (string | number | boolean)[] = [];
-mixed.push(20);
-mixed.push('hello');
-mixed.push(true);
+mixed.push(5);
+mixed.push('mario');
+mixed.push(false);
 console.log(mixed);
 
-let uid: string | number;
-uid = '123';
-uid = 123;
 
-//objects
-let ninjaOne: object;
-ninjaOne = {name: 'yoshi', age: 30};
-
-let ninjaTwo: {
-    name: string,
-    age: number,
-    beltColour: string
-};
-
-ninjaTwo = {name: 'mario', age: 20, beltColour: 'black'}; // it has to have all the properties defined in the object type and not any other properties
